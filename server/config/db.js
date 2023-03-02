c2492dba25266540b2f5c1652bd09c9fe99ec7ca
Mohamed <mohamederwa.11@gmail.com>
2023-03-02 12:06:02 -0500

    Added MongoDb connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://admin:admin@mongodbserver.domqeju.mongodb.net/comp229_Sec004_midterm?retryWrites=true&w=majority"
 };
